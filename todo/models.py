6d8f46d7a17f9a0a914f3716a96d74f0e2a2d03d
PVS1905 <viktor66462@gmail.com>
2025-03-19 14:15:31 +0200

    create models and implement admin.py

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField(max_length=500)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name='tasks')
+
+    class Meta:
+        ordering = ["is_done", "-datetime"]
+
+    def __str__(self):
+        deadline_str = (
+            self.deadline.strftime("%m %d, %I:%M %p")
+            if self.deadline
+            else "Not deadline"
+        )
+        return f"{self.content}: {self.datetime.strftime("%m %d, %I:%M %p")}, deadline: {deadline_str}"
